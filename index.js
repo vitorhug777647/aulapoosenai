35969e1777b7dd2ce477e236558c19d1c81256a1
vitorhug777647 <vitorbraziel21@gmail.com>
2025-05-21 16:51:29 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,63 @@
 const express = require('express')
+const mysql = require('mysql2');
+const cors = require('cors')
+
+
 const app = express()
+
+app.use(cors())
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'loja',
+});
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Servidor Express funcionando!');
+const produtos = []
+
+app.post('/produtos', (req, res) => {
+    const produto = {
+        nome: req.body.nome,
+        preco: req.body.preco,
+        quantidade: req.body.quantidade,
+    }
+
+    if (!produto.nome || typeof produto.nome != 'string' || produto.nome.trim() == '') {
+        return res.status(400).send('Nome do produto é obrigatório e deve ser uma string não vazia.');
+    }
+
+    if (produto.preco == undefined || typeof produto.preco != 'number' || produto.preco <= 0) {
+        return res.status(400).send('Preço deve ser um número positivo.');
+    }
+
+    if (produto.quantidade == undefined || !Number.isInteger(produto.quantidade) || produto.quantidade < 0) {
+        return res.status(400).send('Quantidade deve ser um número inteiro maior ou igual a 0.');
+    }
+
+    conexao.query(
+        'INSERT INTO produtos (nome, preco, quantidade) VALUES (?, ?, ?)',
+        [produto.nome, produto.preco, produto.quantidade],
+        () => {
+            res.status(201).send('produtos cadastrado com sucesso')
+        }
+    );
+
+
 })
 
-app.get('/sobre', (req, res) => {
-    res.send('Bem-vindo à pagina sobre!');
-})
+app.get('/produtos', (req, res) => {
+    conexao.query('SELECT nome, preco, quantidade FROM produtos', (err, results) => {
+        if (err) {
+            return res.status(500).send('Erro ao buscar produtos');
+        }
 
-app.get('/usuario', (req, res) => {
-    res.send('vitor');
-})
+    res.status(200).send(results)
+    });
+});
 
-app.post('/usuario', (req, res) => {
-    const nome = req.body.nome
-    res.send("Usuário " + nome + " criado com susseso")
-})
-
-const curso = ('Programador beck-end')
-
-app.get('/senai', (req, res) => {
-    res.send( curso );
-})
 
 app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000')
+    console.log("Servidor backend rodando em http://localhost:3000")
 })
