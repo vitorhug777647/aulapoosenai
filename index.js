225ca6e49ef16afc4cbab05746198d5b0a8fea97
vitorhug777647 <vitorbraziel21@gmail.com>
2025-05-22 16:25:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,65 @@
 const express = require('express')
 const mysql = require('mysql2');
-const cors = require('cors')
-
 
 const app = express()
 
-app.use(cors())
 const conexao = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'loja',
+    database: 'consultorio_medico',
 });
 
 app.use(express.json())
 
-const produtos = []
+const consulta = []
 
-app.post('/produtos', (req, res) => {
-    const produto = {
-        nome: req.body.nome,
-        preco: req.body.preco,
-        quantidade: req.body.quantidade,
+app.post('/consultas', (req, res) => {
+    const consulta = {
+        paciente: req.body.paciente,
+        medico: req.body.medico,
+        especialidade: req.body.especialidade,
+        data: req.body.data,
+        horario: req.body.horario,
+        observacoes: req.body.observacoes
     }
-
-    if (!produto.nome || typeof produto.nome != 'string' || produto.nome.trim() == '') {
-        return res.status(400).send('Nome do produto é obrigatório e deve ser uma string não vazia.');
-    }
-
-    if (produto.preco == undefined || typeof produto.preco != 'number' || produto.preco <= 0) {
-        return res.status(400).send('Preço deve ser um número positivo.');
-    }
-
-    if (produto.quantidade == undefined || !Number.isInteger(produto.quantidade) || produto.quantidade < 0) {
-        return res.status(400).send('Quantidade deve ser um número inteiro maior ou igual a 0.');
-    }
-
-    conexao.query(
-        'INSERT INTO produtos (nome, preco, quantidade) VALUES (?, ?, ?)',
-        [produto.nome, produto.preco, produto.quantidade],
-        () => {
-            res.status(201).send('produtos cadastrado com sucesso')
-        }
-    );
-
-
-})
-
-app.get('/produtos', (req, res) => {
-    conexao.query('SELECT nome, preco, quantidade FROM produtos', (err, results) => {
-        if (err) {
-            return res.status(500).send('Erro ao buscar produtos');
-        }
-
-    res.status(200).send(results)
-    });
 });
 
+    conexao.query(
+        'INSERT INTO consulta (paciente, medico, especialidade, data, horario, observacoes) VALUES (?, ?, ?, ?, ?, ?)',
+        [consulta.paciente, consulta.medico, consulta.especialidade, consulta.data, consulta.horario, consulta.observacoes],
+        () => {
+            res.status(201).send('a consulta foi achada')
+        }
 
-app.listen(3000, () => {
-    console.log("Servidor backend rodando em http://localhost:3000")
-})
+    );
+ 
+    app.get('/consultas', (req, res) => {
+        conexao.query('SELECT paciente, medico, especialidade, data, horario, observacoes FROM consulta', (err, results) => {
+            if (err) {
+                return res.status(500).send('a consulta não foi achada');
+            }
+            res.status(200).send(results)
+        });
+    });  
+
+     app.listen(3000, () => {
+        console.log("Servidor backend rodando em http://localhost:3000")
+    })
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
